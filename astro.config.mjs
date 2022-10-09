94b1e1d48f27a0085cab0ec468d8ca53a01a7213
andrew <andrew_zachary@outlook.com>
2022-10-09 00:00:00 +0000

    setup styles/scripts/assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,31 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            $primary-color: #ff9292;
+            $secondary-color: #ffdd67;
+            $tertiary-color: #403f3f;
+            $quaternary-color: white;
+            @mixin withRtl($property, $ltr-value, $rtl-value) {
+              & {
+                #{$property}: $ltr-value;
+              }
+          
+              [dir='rtl'] & {
+                #{$property}: $rtl-value;
+              }
+            }
+          `
+        }
+      }
+    }
+  }
+});
