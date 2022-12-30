51f4985a42efe0f863ecf9436759924a926636d9
andrew-zachary <andrew_zachary@outlook.com>
2022-12-30 00:00:00 +0000

    global class btn-icon-txt and btn-txt-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ export default defineConfig({
   integrations: [tailwind()],
   vite: {
     css: {
+      devSourcemap: false,
       preprocessorOptions: {
         scss: {
           additionalData: `
