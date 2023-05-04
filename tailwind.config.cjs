96d92f868576c7dee0e8059ba059886203f307f5
andrew-zachary <andrew_zachary@outlook.com>
2023-05-04 00:00:00 +0000

    update templates

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-
-const defaultTheme = require('tailwindcss/defaultTheme');
-
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
 			screens: {
-				...defaultTheme.screens,
 				xlg: '1200px',
 				lg: '992px',
 				md: '768px',
@@ -23,31 +19,6 @@ module.exports = {
 				'9997': '9997',
 				'9996': '9996',
 				'9995': '9995'
-			},
-			colors: {
-				'wlk-primary-color': '#ff9292',
-				'wlk-secondary-color': '#ffdd67',
-				'wlk-tertiary-color': '#403f3f',
-				'wlk-quaternary-color': 'white',
-				'wlk-dim-modal': '#403f3fcc',
-				'hanv-primary-color': '#3157AD',
-				'hanv-secondary-color': '#EDE5DA'
-			},
-			boxShadow: {
-				'wlk-primary': '0rem 1.7rem 2.016rem 0.384rem rgba(0, 0, 0, 0.21)'
-			},
-			fontFamily: {
-				mont: ['Montserrat'],
-				hmPop: ['Hachi Maru Pop']
-			},
-			fontWeight: {
-				thin: 100,
-				'extra-light': 200,
-				light: 300,
-				regular: 400,
-				medium: 500,
-				bold: 700,
-				black: 900
 			}
 		},
 	},
