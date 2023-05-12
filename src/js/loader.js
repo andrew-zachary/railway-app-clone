ad2b14c3a7841775a05e7b8107b24b4ebbea064f
andrew-zachary <andrew_zachary@outlook.com>
2023-05-12 00:00:00 +0000

    add green-trains within section-iterate-accelerate and adjust trains offset while moving

--- src/js/loader.js
+++ src/js/loader.js
@@ -6,10 +6,19 @@ const loader = (page) => {
     const trainsCtrl = new ScrollMagic.Controller();
 
     let blueTrain = document.getElementById("blueTrain");
+    let greenTrain1 = document.getElementById("greenTrain1");
+    let greenTrain2 = document.getElementById("greenTrain2");
+    let greenTrain3 = document.getElementById("greenTrain3");
 
-    let scene = new ScrollMagic.Scene({triggerElement: "#blueTrainStation", duration: 350})
-    .setPin(blueTrain)
-    .addTo(trainsCtrl);
+    let scene1 = new ScrollMagic.Scene({triggerElement: "#blueTrainStation", duration: 350});
+    scene1.setPin(blueTrain).addTo(trainsCtrl);
+
+    let scene2 = new ScrollMagic.Scene({triggerElement: "#greenTrainStation", duration: 350, offset: 100});
+    scene2.setPin(greenTrain1).addTo(trainsCtrl);
+    let scene3 = new ScrollMagic.Scene({triggerElement: "#greenTrainStation", duration: 350});
+    scene3.setPin(greenTrain2).addTo(trainsCtrl);
+    let scene4 = new ScrollMagic.Scene({triggerElement: "#greenTrainStation", duration: 350, offset: 50});
+    scene4.setPin(greenTrain3).addTo(trainsCtrl);
     
     // navmenu alpinejs context
     alpine.data('navmenu', () => ({
