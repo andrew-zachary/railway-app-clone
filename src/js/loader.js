38c937bece08c1d0c651f47029853f3ef35908fc
andrew-zachary <andrew_zachary@outlook.com>
2023-05-18 00:00:00 +0000

    reveal lists animations start-tinker/iterate-accelerate/launch-scale sections

--- src/js/loader.js
+++ src/js/loader.js
@@ -10,22 +10,34 @@ const loader = (page) => {
     const greenTrain2 = document.getElementById('greenTrain2');
     const greenTrain3 = document.getElementById('greenTrain3');
     const bigTrain = document.getElementById('bigTrain');
+    const startTinkerLists = document.querySelectorAll('.start-tinker-lists');
+    const iterateAccelerateLists = document.querySelectorAll('.iterate-accelerate-lists');
+    const launchScaleLists = document.querySelectorAll('.launch-scale-lists');
 
-    const scene1 = new ScrollMagic.Scene({triggerElement: '#blueTrainStation', duration: 350});
-    scene1.setPin(blueTrain).addTo(trainsCtrl);
+    const blueTrainScene = new ScrollMagic.Scene({triggerElement: '#blueTrainStation', duration: 350});
+    blueTrainScene.setPin(blueTrain).addTo(trainsCtrl);
 
-    const scene2 = new ScrollMagic.Scene({triggerElement: '#greenTrainStation', duration: 350, offset: 100});
-    scene2.setPin(greenTrain1).addTo(trainsCtrl);
-    const scene3 = new ScrollMagic.Scene({triggerElement: '#greenTrainStation', duration: 350});
-    scene3.setPin(greenTrain2).addTo(trainsCtrl);
-    const scene4 = new ScrollMagic.Scene({triggerElement: '#greenTrainStation', duration: 350, offset: 50});
-    scene4.setPin(greenTrain3).addTo(trainsCtrl);
+    const greenTrain1Scene = new ScrollMagic.Scene({triggerElement: '#greenTrainStation', duration: 350, offset: 100});
+    greenTrain1Scene.setPin(greenTrain1).addTo(trainsCtrl);
+    const greenTrain2Scene = new ScrollMagic.Scene({triggerElement: '#greenTrainStation', duration: 350});
+    greenTrain2Scene.setPin(greenTrain2).addTo(trainsCtrl);
+    const greenTrain3Scene = new ScrollMagic.Scene({triggerElement: '#greenTrainStation', duration: 350, offset: 50});
+    greenTrain3Scene.setPin(greenTrain3).addTo(trainsCtrl);
 
-    const scene5 = new ScrollMagic.Scene({triggerElement: '#launch-scale', duration: 500});
-    scene5.addTo(trainsCtrl).on('progress', (e) => {
+    const bigTrainScene = new ScrollMagic.Scene({triggerElement: '#launch-scale', duration: 500});
+    bigTrainScene.addTo(trainsCtrl).on('progress', (e) => {
         const scaleAmount = parseFloat(e.progress.toFixed(2)) + 0.5;
         bigTrain.style.transform = `scale(${scaleAmount})`;
     });
+
+    const startAndTinkerScene = new ScrollMagic.Scene({triggerElement: '#start-tinker-box', offset: -235});
+    startAndTinkerScene.addTo(trainsCtrl).setClassToggle(startTinkerLists, 'reveal');
+
+    const iterateAccelerateScene = new ScrollMagic.Scene({triggerElement: '#iterate-accelerate-box', offset: -235});
+    iterateAccelerateScene.addTo(trainsCtrl).setClassToggle(iterateAccelerateLists, 'reveal');
+
+    const launchScaleScene = new ScrollMagic.Scene({triggerElement: '#launch-scale-box', offset: -235});
+    launchScaleScene.addTo(trainsCtrl).setClassToggle(launchScaleLists, 'reveal');
     
     // navmenu alpinejs context
     alpine.data('navmenu', () => ({
